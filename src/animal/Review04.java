f7dd834de74a2d1e3a140a9fbc694edc8ec2aa9f
takeshi.hisatomi <take_tomy7@yahoo.co.jp>
2024-10-19 19:48:04 +0900

    kadai-java-review04修正版 https://techacademy.jp/my/ly-jobchange/ly-jobchange/review/2648

--- src/animal/Review04.java
+++ src/animal/Review04.java
@@ -3,15 +3,15 @@ package animal;
 public class Review04 {
 
     public static void main(String[] args) {
-        Human Tanaka = new Human("田中 太郎", 25, "電車");
-        Human Suzuki = new Human("鈴木 次郎", 30, "野球");
-        Human Sato = new Human("佐藤 花子", 20, "映画");
+        Human tanaka = new Human("田中 太郎", 25, "電車");
+        Human suzuki = new Human("鈴木 次郎", 30, "野球");
+        Human sato = new Human("佐藤 花子", 20, "映画");
 
-        Tanaka.say();
-        Tanaka.think();
-        Suzuki.say();
-        Suzuki.think();
-        Sato.say();
-        Sato.think();
+        tanaka.say();
+        tanaka.think();
+        suzuki.say();
+        suzuki.think();
+        sato.say();
+        sato.think();
     }
 }
